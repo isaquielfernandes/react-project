440fa9fbbbb8ab8fec485315598b2a13448582cf
IF <iisaquielfernandes@gmail.com>
2020-06-09 17:02:54 +0000

    Update CartTotals.js

--- store/src/components/Cart/CartTotals.js
+++ store/src/components/Cart/CartTotals.js
@@ -16,13 +16,7 @@ export default class CartTotals extends Component {
             <div className="row">
               <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
                 <Link to="/">
-                  <button
-                    className="btn btn-outline-danger text-uppercase mb-3 px-5"
-                    type="button"
-                    onClick={() => {
-                      clearCart();
-                    }}
-                  >
+                  <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => { clearCart(); }} >
                     clear cart
                   </button>
                 </Link>
