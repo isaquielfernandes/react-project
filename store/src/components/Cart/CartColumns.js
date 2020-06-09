8dbb3222c4ef777327c297ef1864ea019a93a965
IF <iisaquielfernandes@gmail.com>
2020-06-09 16:44:24 +0000

    Update CartColumns.js

--- store/src/components/Cart/CartColumns.js
+++ store/src/components/Cart/CartColumns.js
@@ -1,6 +1,6 @@
-import React, { Componentes } from "react";
+import React, { Component } from "react";
 
-export default class CartColumns extensa Componentes {
+export default class CartColumns extends Component {
    render() {
        return (
           <div className="container-fluid text-center d-none d-lg-block">
