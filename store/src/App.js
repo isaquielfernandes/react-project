eb8df249ae1fac36d3e69113474448c559bf4202
IF <iisaquielfernandes@gmail.com>
2020-06-09 17:12:49 +0000

    Update App.js

--- store/src/App.js
+++ store/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+//import 'primereact/resources/themes/nova-light/theme.css';
+//import 'primereact/resources/primereact.min.css';
+//import 'primeicons/primeicons.css';
 import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 import { Route, Switch } from "react-router-dom";
 import Details from "./components/Detalis";
 import Default from "./components/Default";
 import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={ProductList}/>
           <Route exact path="/details" component={Details}/>
+          <Route exact path="/cart" component={Cart}/>
           <Route component={Default}/>
         </Switch>
         <Modal/>
