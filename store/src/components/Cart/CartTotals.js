7a4118bb3cc6f4bb707d25d905077780ccbf7a0e
IF <iisaquielfernandes@gmail.com>
2020-06-09 17:01:07 +0000

    Update CartTotals.js

--- store/src/components/Cart/CartTotals.js
+++ store/src/components/Cart/CartTotals.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import PayPalButton from "./PayPalButton";
 import { Link } from "react-router-dom";
+
 export default class CartTotals extends Component {
   render() {
+
     const { cartSubTotal, cartTax, cartTotal, cart, clearCart } = this.props.value;
     const { history } = this.props;
     const emptyCart = cart.length === 0 ? true : false;
