589f89c27d221cab0080d7de6a23cb58d0f3d09c
IF <iisaquielfernandes@gmail.com>
2020-06-15 11:02:50 +0000

    Create SignIn.js

---
+++ store/src/components/SignIn.js
@@ -0,0 +1,66 @@
+// Import FirebaseAuth and firebase.
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+ 
+// Configure Firebase.
+const config = {
+  apiKey: 'AIzaSyBlgOzvwtY-nb-0sDeMoG2KnqdykhLxlcA',
+  authDomain: 'https://store-iota.now.sh',
+  // ...
+};
+firebase.initializeApp(config);
+ 
+export default class SignInScreen extends React.Component {
+ 
+  // The component's Local state.
+  state = {
+    isSignedIn: false // Local signed-in state.
+  };
+ 
+  // Configure FirebaseUI.
+  uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      // Avoid redirects after sign-in.
+      signInSuccessWithAuthResult: () => false
+    }
+  };
+ 
+  // Listen to the Firebase Auth state and set the local state.
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+        (user) => this.setState({isSignedIn: !!user})
+    );
+  }
+  
+  // Make sure we un-register Firebase observers when the component unmounts.
+  componentWillUnmount() {
+    this.unregisterAuthObserver();
+  }
+ 
+  render() {
+    if (!this.state.isSignedIn) {
+      return (
+        <div>
+          <h1>My App</h1>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h1>My App</h1>
+        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+        <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+      </div>
+    );
+  }
+}
