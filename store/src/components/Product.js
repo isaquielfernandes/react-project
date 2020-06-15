3d6d58e893e9bc6a8bfb5d37c70f305897508615
IF <iisaquielfernandes@gmail.com>
2020-06-15 22:52:02 +0000

    Update Product.js

--- store/src/components/Product.js
+++ store/src/components/Product.js
@@ -7,7 +7,7 @@ export default class Product extends Component {
     render() {
         const { id, title, img, price, inCart } = this.props.product;
         return(
-            <ProductWrapper className="col-6 mx-auto col-md-6 col-lg-3 my-1">
+            <ProductWrapper className="col-10 mx-auto col-md-6 col-lg-3 my-1">
                 <div className="card">
                 <ProductConsumer>
                     {value => {
@@ -36,8 +36,8 @@ export default class Product extends Component {
                 <div className="card-footer d-flex justify-content-between">
                     <p className="align-self-center mb-0">{title}</p>
                     <h5 className="text-blue font-italic mb-0">
-                    <span className="mr-1">$</span>
-                    {price}
+                       <span className="mr-1">$</span>
+                       {price}
                     </h5>
                 </div>
                 </div>
