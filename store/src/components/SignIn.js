aafa2a2325fb267d4fdd60f99e779e320ced1cce
IF <iisaquielfernandes@gmail.com>
2020-06-16 10:17:31 +0100

    Update SignIn.js

--- store/src/components/SignIn.js
+++ store/src/components/SignIn.js
@@ -6,7 +6,6 @@ import firebase from 'firebase/auth';
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // ...
 };
 firebase.initializeApp(config);
  
