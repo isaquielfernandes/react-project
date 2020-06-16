66d01c31309e368921c6e2a5f5be4c0a4d20cc62
IF <iisaquielfernandes@gmail.com>
2020-06-16 10:08:12 +0100

    Update Product.js

--- store/src/components/Product.js
+++ store/src/components/Product.js
@@ -7,39 +7,39 @@ export default class Product extends Component {
     render() {
         const { id, title, img, price, inCart } = this.props.product;
         return(
-            <ProductWrapper className="col-10 mx-auto col-md-6 col-lg-3 my-1">
+            <ProductWrapper className="col-10 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                <ProductConsumer>
-                    {value => {
-                    return (
-                        <div className="img-container p-5" onClick={() => value.handleDetail(id)}>
-                            <Link to="/details">
-                                <img src={img} alt="" className="card-img-top" />
-                            </Link>
-                            <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {
-                                value.addToCart(id);
-                                value.openModal(id);
-                                }}>
-                                    
-                                {inCart ? (
-                                <p className="text-capitalize mb-0" disabled>
-                                    in cart
-                                </p>
-                                ) : (
-                                <i className="fas fa-cart-plus" />
-                                )}
-                            </button>
-                        </div>
-                    );
-                    }}
-                </ProductConsumer>
-                <div className="card-footer d-flex justify-content-between">
-                    <p className="align-self-center mb-0">{title}</p>
-                    <h5 className="text-blue font-italic mb-0">
-                       <span className="mr-1">$</span>
-                       {price}
-                    </h5>
-                </div>
+                  <ProductConsumer>
+                      {value => {
+                      return (
+                          <div className="img-container p-5" onClick={() => value.handleDetail(id)}>
+                              <Link to="/details">
+                                  <img src={img} alt="" className="card-img-top" />
+                              </Link>
+                              <button className="cart-btn" disabled={inCart ? true : false} onClick={() => {
+                                  value.addToCart(id);
+                                  value.openModal(id);
+                                  }}>
+                                      
+                                  {inCart ? (
+                                  <p className="text-capitalize mb-0" disabled>
+                                      in cart
+                                  </p>
+                                  ) : (
+                                  <i className="fas fa-cart-plus" />
+                                  )}
+                              </button>
+                          </div>
+                      );
+                      }}
+                  </ProductConsumer>
+                  <div className="card-footer d-flex justify-content-between">
+                      <p className="align-self-center mb-0">{title}</p>
+                      <h5 className="text-blue font-italic mb-0">
+                        <span className="mr-1">$</span>
+                        {price}
+                      </h5>
+                  </div>
                 </div>
             </ProductWrapper>
         );
