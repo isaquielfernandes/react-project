f7c3064fe24d5c31efbbb617d015043e594da646
IF <iisaquielfernandes@gmail.com>
2020-06-17 19:04:58 +0000

    Update CartList.js

--- store/src/components/Cart/CartList.js
+++ store/src/components/Cart/CartList.js
@@ -8,7 +8,7 @@ export default class CartList extends Component {
        const { cart } = this.props.value;
 
        return (
-           <div className="container-fluid">
+           <div className="container">
              {cart.map(item => ( <CartItem key={item.id} item={item} value={value} /> ))}
            </div>
        );
