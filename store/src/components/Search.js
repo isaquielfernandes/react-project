0fe9b5000cdf887cfe0a335682b9caab09e10a84
IF <iisaquielfernandes@gmail.com>
2020-06-18 16:27:41 +0000

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class Search extends Component {
     render() {
        return (
-          <nav className="navbar navbar-light bg-light">
+          <nav className="">
              <form className="form-inline">
                  <input className="col-8 form-control mx-auto" type="search" placeholder="Search" aria-label="Search"/>
                  <button className="col-4 btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
