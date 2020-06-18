b1a6ed7ca4cc722af28d7744009230909285a909
IF <iisaquielfernandes@gmail.com>
2020-06-18 16:24:47 +0000

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -5,7 +5,7 @@ export default class Search extends Component {
        return (
           <nav className="navbar navbar-light bg-light">
              <form className="form-inline">
-                 <input className="col-8 form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                 <input className="col-8 form-control mx-auto" type="search" placeholder="Search" aria-label="Search"/>
                  <button className="col-4 btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
              </form>
          </nav>
