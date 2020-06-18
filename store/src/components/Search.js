928b12fc90209620c301d92d76e5d7c81f6592d2
IF <iisaquielfernandes@gmail.com>
2020-06-18 17:40:38 +0000

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -8,7 +8,7 @@ export default class Search extends Component {
                      <div className="form-group p-0 col-8">
                          <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
                      </div>
-                     <div className="form-group ml-1 p-0 col-4">
+                     <div className="form-group ml-auto p-0 col-4">
                          <button className="btn btn-block btn-outline-success my-sm-0" type="submit">Search</button>
                      </div>
                  </div>
