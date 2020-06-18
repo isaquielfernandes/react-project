294267fccdd7859896b83a80eed4277a6b4e654d
IF <iisaquielfernandes@gmail.com>
2020-06-18 16:36:23 +0000

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -3,9 +3,9 @@ import React, {Component} from "react";
 export default class Search extends Component {
     render() {
        return (
-          <nav className="col-10">
-             <form className="form-inline">
-                 <input className="col-9 form-control mx-auto" type="search" placeholder="Search" aria-label="Search"/>
+          <nav className="col-12">
+             <form className="form-inline row">
+                 <input className="col-9 form-control" type="search" placeholder="Search" aria-label="Search"/>
                  <button className="col-3 ml-1 btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
              </form>
          </nav>
