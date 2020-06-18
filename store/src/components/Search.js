467761c4997617f1ceaf3f8163d649fab983acc7
IF <iisaquielfernandes@gmail.com>
2020-06-18 17:15:26 +0000

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -3,11 +3,11 @@ import React, {Component} from "react";
 export default class Search extends Component {
     render() {
        return (
-             <form> 
-                 <div className="col-9 form-group">
-                 <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+             <form className="row"> 
+                 <div className="form-group">
+                     <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
                  </div>
-                 <div className="col-3 form-group">
+                 <div className="form-group">
                     <button className="btn btn-block btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                  </div>
              </form>
