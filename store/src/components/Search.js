42428c4d6946310568dae26cc48fc31658755063
IF <iisaquielfernandes@gmail.com>
2020-06-18 17:26:36 +0000

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -4,11 +4,13 @@ export default class Search extends Component {
     render() {
        return (
              <form className="container"> 
-                 <div className="form-group col-8">
-                     <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
-                 </div>
-                 <div className="form-group col-4">
-                    <button className="btn btn-block btn-outline-success my-sm-0" type="submit">Search</button>
+                 <div className="row">
+                     <div className="form-group col-8">
+                         <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                     </div>
+                     <div className="form-group col-4">
+                         <button className="btn btn-block btn-outline-success my-sm-0" type="submit">Search</button>
+                     </div>
                  </div>
              </form>
        )
