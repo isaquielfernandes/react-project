8b5c3a76ebdfbdd577da9a56b2b8d6405573f0bb
IF <iisaquielfernandes@gmail.com>
2020-06-21 11:05:50 +0000

    Update CartTotals.js

--- store/src/components/Cart/CartTotals.js
+++ store/src/components/Cart/CartTotals.js
@@ -15,6 +15,7 @@ export default class CartTotals extends Component {
           <div className="container">
             <div className="row">
               <div className="col-10 mt-2 ml-sm-4 ml-md-auto mb-5 col-sm-8 text-capitalize text-right">
+                <hr/>
                 <Link to="/">
                   <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => { clearCart(); }} >
                     clear cart
@@ -32,6 +33,7 @@ export default class CartTotals extends Component {
                   <span className="text-title"> total :</span>{" "}
                   <strong>$ {cartTotal} </strong>
                 </h5>
+                <hr/>
                 <PayPalButton
                   total={cartTotal}
                   clearCart={clearCart}
