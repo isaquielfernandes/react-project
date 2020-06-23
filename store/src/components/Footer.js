1e6718d862ac40f891b86013a91e3381474580c0
IF <iisaquielfernandes@gmail.com>
2020-06-23 16:25:53 +0100

    Update Footer.js

--- store/src/components/Footer.js
+++ store/src/components/Footer.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="p-3 mb-2 bg-secondary text-white">
+                <hr/>
+                <div>
+                    <P>loremmmmmmmmmmmmm</P>
+                </div>
+                <div>
+                    
+                </div>
             </div>
         );
     }
