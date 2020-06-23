8b9dd3290124061b234cfe7626d914e7385d1500
IF <iisaquielfernandes@gmail.com>
2020-06-23 15:52:57 +0100

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export default class Search extends Component {
     render() {
        return (
             <form className="container"> 
                 <div className="form-row">
-                    <div class="col-md-4 offset-md-4">
+                    <div class="col-md-6">
                         <div class="col-md-6 mb-3">
                             <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
                         </div>
