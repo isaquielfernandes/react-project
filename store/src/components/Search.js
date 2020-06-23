bf42cda19e1455e5c3d05dc6f677f32cc65b3563
IF <iisaquielfernandes@gmail.com>
2020-06-23 15:10:19 +0100

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -5,7 +5,7 @@ export default class Search extends Component {
        return (
             <form className="container"> 
                 <div className="row">
-                    <div class="col-md-4 offset-md-4">
+                    <div class="col-md-6 offset-md-4">
                         <div className="form-group p-1 col-10">
                             <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
                         </div>
