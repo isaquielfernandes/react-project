3b7ad13c74891872002894e1082017e2928f64b4
IF <iisaquielfernandes@gmail.com>
2020-06-24 11:50:37 +0100

    Update Footer.js

--- store/src/components/Footer.js
+++ store/src/components/Footer.js
@@ -6,10 +6,15 @@ class Footer extends Component {
             <div className="p-3 mt-3 bg-secondary text-white">
                 <hr/>
                 <div className="py-2">
-                    
+                    <h3></h3>
+                    <p></p>
+                    <p></p>
+                    <p></p>
                 </div>
                 <div className="py-2">
-                    
+                    <a></a>
+                    <a></a>
+                    <a></a>
                 </div>
             </div>
         );
