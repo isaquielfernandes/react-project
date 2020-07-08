2ff5871b1a995139ce9805f2cff606263773ce11
IF <iisaquielfernandes@gmail.com>
2020-07-08 20:46:11 +0000

    Update Search.js

--- store/src/components/Search.js
+++ store/src/components/Search.js
@@ -1,13 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-const Search = () => {
+const Search = ({ getQuery }) => {
+
+       const [text, setText] = useState('')
+
+       const onChange = (q) => {
+          setText(q)
+          getQuery(q)
+       }
+
        return (
             <form className="mt-2"> 
                 <div className="form-row my-3 mx-auto">
                     <div className="col-12">
                        <label className="sr-only" for="inlineFormInputGroup">search</label>
                        <div className="input-group mb-2">
-                          <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                          <input 
+                             className="form-control" 
+                             type="search" 
+                             placeholder="Search" 
+                             aria-label="Search"
+                             value={text}
+                             onChange={(e) => onChange(e.target.value)}
+                             autoFocus
+                          />
                           <div className="input-group-prepend">
                               <div className="input-group-text">
                                     <i className="fas fa-search"></i>
